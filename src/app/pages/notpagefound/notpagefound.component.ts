1ad5103f6dbe791d81f0e3a1c3c2867a98770246
Mauricio Latorre <latorre.mauricio.1992@gmail.com>
2023-01-17 21:26:05 -0300

    Se agregan  nuevas funcionalidades

--- src/app/pages/notpagefound/notpagefound.component.ts
+++ src/app/pages/notpagefound/notpagefound.component.ts
@@ -3,14 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notpagefound',
   templateUrl: './notpagefound.component.html',
-  styles: [
+  styleUrls: [
+    './nopagefound.component.css'
   ]
 })
-export class NotpagefoundComponent implements OnInit {
+export class NotpagefoundComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  year = new Date().getFullYear();
 
 }
